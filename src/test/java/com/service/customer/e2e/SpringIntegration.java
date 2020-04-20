a0176046de298e37b526614ebd281f6fd2139ac2
abhisheksr01 <abhisheksingh2691@gmail.com>
2020-04-20 15:01:56 +0100

    Implement embed test db for testing e2e and IT

--- src/test/java/com/service/customer/e2e/SpringIntegration.java
+++ src/test/java/com/service/customer/e2e/SpringIntegration.java
@@ -1,6 +1,7 @@
 package com.service.customer.e2e;
 
 import com.service.customer.CustomerApplication;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
         classes = CustomerApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
+@AutoConfigureEmbeddedDatabase
 public abstract class SpringIntegration {
     protected static final String DEFAULTURL = "http://localhost:8081/customer/";
     protected RestTemplate restTemplate = new RestTemplate();
---
+++ src/test/java/com/service/customer/repository/CustomerRepositoryIT.java
@@ -0,0 +1,46 @@
+package com.service.customer.repository;
+
+import com.service.customer.model.Customer;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.service.customer.helper.TestData.getCustomerData;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureEmbeddedDatabase
+@ActiveProfiles("test")
+public class CustomerRepositoryIT {
+
+    @Autowired
+    private CustomerRepository personRepository;
+    private Customer customer = getCustomerData();
+
+    @Test
+    public void save_whenCustomerDataIsPassed_shouldStoreTheCustomerData() {
+        customer.setFirstName("Iron");
+        customer.setSurname("Man");
+
+        Customer storedCustomer = personRepository.save(customer);
+
+        assertEquals(storedCustomer.getFirstName(), "Iron");
+        assertEquals(storedCustomer.getSurname(), "Man");
+    }
+
+    @Test
+    public void getOne_whenCustomerIdIsPassed_shouldReturnTheCustomerData() {
+        customer.setFirstName("Steve");
+        customer.setSurname("Rogers");
+        Customer storedCustomer = personRepository.save(customer);
+
+        Customer actualCustomer = personRepository.getOne(customer.getId());
+
+        assertEquals(customer, actualCustomer);
+    }
+}
